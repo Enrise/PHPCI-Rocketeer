cfcd84959838ab9e334850e673f43c4a2a790766
Rick van der Staaij <RickvdStaaij@users.noreply.github.com>
2015-07-11 00:19:11 +0200

    Update plugin with Enrise credits

--- src/RocketeerPlugin.php
+++ src/RocketeerPlugin.php
@@ -1,12 +1,12 @@
 <?php
 /**
+ * Rocketeer deployment module for PHPCI
  * PHPCI - Continuous Integration for PHP
  *
- * @copyright    Copyright 2014, Block 8 Limited.
- * @license      https://github.com/Block8/PHPCI/blob/master/LICENSE.md
- * @link         https://www.phptesting.org/
+ * @copyright Copyright 2015, Enrise
+ * @license https://github.com/Enrise/PHPCI-Rocketeer/blob/master/LICENSE
+ * @link https://github.com/Enrise/PHPCI-Rocketeer
  */
-
 namespace Enrise\PHPCI\Plugins;
 
 use PHPCI\Builder;
